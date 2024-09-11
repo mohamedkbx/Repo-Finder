afb3b7415ceb5bfea9df314b32288f81c7e6bd7d
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-11 12:56:54 +0300

    feat: add Search Logic -Handle DOM Elements with the Cards

--- script.js
+++ script.js
@@ -10,18 +10,31 @@
 
 const USERS_API = "http://api.github.com/search/users?q=";
 
-const setSearchResult = () => {};
+const cardElements = document.querySelector(".cards");
+
+const setSearchResult = (data) => {
+  let result = "";
+  data.map((item) => {
+    result += `<article class="card">
+    <img src="${item.avatar_url}" class="img" loading="lazy"/>
+    <h2 class="name">${item.login}</h2>
+    </article>`;
+  });
+  cardElements.innerHTML = result;
+};
 
 const performSearch = (searchTerm, isUserSelected) => {
   const typyQuery = isUserSelected ? "+type:user" : "+type:org";
   fetch(`${USERS_API}${searchTerm}${typyQuery}`)
     .then((response) => response.json())
-    .then((response) => setSearchResult(response));
+    .then((response) => setSearchResult(response.items));
 };
 
 const buttonElement = document.querySelector(".button");
+const searchInput = document.querySelector(".input");
+const userInputElement = document.querySelector("input[value='users']");
 
 buttonElement?.addEventListener("click", (e) => {
   e.preventDefault();
-  performSearch();
+  performSearch(searchInput.value, userInputElement.checked);
 });
