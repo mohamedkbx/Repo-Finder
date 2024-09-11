312e72eb77e5334bdc99bccd108c996df6746065
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-11 21:45:38 +0300

    refactor: handle research UX

--- scripts/setLoadingState.js
+++ scripts/setLoadingState.js
@@ -1,5 +1,7 @@
 import { loadingElement } from "./elements.js";
+import { setSearchResult } from "./setSearchResult.js";
 
 export default function setLoadingState(state) {
   loadingElement.classList.toggle("hidden", !state);
+  state && setSearchResult(null);
 }
--- scripts/setSearchResult.js
+++ scripts/setSearchResult.js
@@ -2,11 +2,16 @@ import { cardElements } from "./elements";
 
 export const setSearchResult = (data) => {
   let result = "";
-  data.map((item) => {
-    result += `<article class="card">
+  if (data === null) {
+    result = "";
+  } else if (data?.length) {
+    data.map((item) => {
+      result += `<article class="card">
         <img src="${item.avatar_url}" class="img" loading="lazy"/>
         <h2 class="name">${item.login}</h2>
         </article>`;
-  });
+    });
+  }
+
   cardElements.innerHTML = result;
 };
